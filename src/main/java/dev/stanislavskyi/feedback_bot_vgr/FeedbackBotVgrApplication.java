d7528636e97a9c31fb473f234c8fc8d184e16e09
Roman <amadeusmozart1a@gmail.com>
2025-08-30 22:30:53 +0300

    Initial commit

---
+++ src/main/java/dev/stanislavskyi/feedback_bot_vgr/FeedbackBotVgrApplication.java
@@ -0,0 +1,13 @@
+package dev.stanislavskyi.feedback_bot_vgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackBotVgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackBotVgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stanislavskyi/feedback_bot_vgr/FeedbackBotVgrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stanislavskyi.feedback_bot_vgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackBotVgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
